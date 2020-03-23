f9e6b1d6e1767dbf51de21c551ddac91ef8fa836
Amanda Hogan <ahogan@teamworks.com>
2020-03-23 10:36:10 -0400

    Add password to seed data

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'batman'},
-        {id: 2, username: 'sparta'},
-        {id: 3, username: 'Panic!atTheDisco'}
+        {id: 1, username: 'batman', password: 'iam'},
+        {id: 2, username: 'sparta', password: 'dog'},
+        {id: 3, username: 'Panic!atTheDisco', password: 'jammingallday'}
       ]);
     });
 };
