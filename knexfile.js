230b87571f63a460037efcfd6d462fe83863ea9a
Amanda Hogan <ahogan@teamworks.com>
2020-03-24 19:42:51 -0400

    Postgres setup

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,14 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     useNullAsDefault: true,
     connection: {
-      filename: './data/users.db3'
+      host: '127.0.0.1',
+      username: 'postgres',
+      password: process.env.DB_URL,
+      database: 'auth'
+      // filename: './data/users.db3'
     },
     migrations: {
       directory: './data/migrations'
@@ -15,9 +19,36 @@ module.exports = {
       directory: './data/seeds'
     },
     pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      }
+      min: 2,
+      max: 10
+      // afterCreate: (conn, done) => {
+      //   conn.run('PRAGMA foreign_keys = ON', done);
+      // }
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      username: username,
+      password: password,
+      database: 'my_db'
+      // filename: './data/users.db3'
+    },
+    pool: {
+      min: 2,
+      max: 10
+      // afterCreate: (conn, done) => {
+      //   conn.run('PRAGMA foreign_keys = ON', done);
+      // }
+    },
+    migrations: {
+      directory: './data/migrations',
+      filename: 'knex_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 
